dbf9a1435927cc45c0b23060c5560c60558dd0a9
Johan Nilsson <johan.nilsson@chromaway.com>
2024-05-13 10:20:21 +0200

    Stability fix for integration tests

--- src/test/resources/net/postchain/eif/bad/evm/geth/start.sh
+++ src/test/resources/net/postchain/eif/bad/evm/geth/start.sh
@@ -2,4 +2,4 @@
 geth --datadir /ethereum/data --password /geth/password account import /geth/key
 geth --datadir /ethereum/data --http --http.addr=0.0.0.0 --http.vhosts=* \
     --dev --dev.period=1 --rpc.allow-unprotected-txs --password /geth/password \
-    --allow-insecure-unlock --unlock 0xe105ba42b66d08ac7ca7fc48c583599044a6dab3
+    --allow-insecure-unlock --unlock 0xe105ba42b66d08ac7ca7fc48c583599044a6dab3 # --log.debug --verbosity 5 --vmdebug
